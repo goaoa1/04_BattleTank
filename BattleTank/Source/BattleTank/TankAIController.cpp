b98889e0e33a8964c05db299f24e5388fed899ea
goaoa <goaoa@naver.com>
2019-04-05 19:23:28 +0900

    aft lec144

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankAIController BeginPlay"))
+
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController can't find PlayerTank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player: %s"), *(PlayerTank->GetName()))
+	}
+
+
+}
+
+
+
+ATank* ATankAIController::GetControlledTank() const //const is also a signiture
+{
+
+
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	/*
+	ATankPlayerController* TankPlayerController = Cast<ATankPlayerController>(GetWorld()->GetFirstPlayerController());
+	ATank* PlayerTank = TankPlayerController->GetControlledTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No PlayerTank"))
+		return nullptr;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Got PlayerTank"))
+		return PlayerTank;
+	}
+	My way*/
+
+
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+
+
+
+
+}
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,9 +3,28 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay"))
+
+
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController has no pawn"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController has pawn %s"), *(ControlledTank->GetName()))
+	}
+}
+
 
 
 ATank* ATankPlayerController::GetControlledTank() const //const is also a signiture
 {
+	
+
 	return Cast<ATank>(GetPawn());
 }
